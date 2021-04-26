d8b077a8799f0a900713f31584faef451821b65d
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-04-26 19:45:30 +0200

    Edited spring security config to disable csrf

--- src/main/java/pl/krystian/security/SecurityConfig.java
+++ src/main/java/pl/krystian/security/SecurityConfig.java
@@ -9,6 +9,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/").permitAll();
+		http.authorizeRequests().antMatchers("/").permitAll().and().csrf().disable();
 	}
 }
